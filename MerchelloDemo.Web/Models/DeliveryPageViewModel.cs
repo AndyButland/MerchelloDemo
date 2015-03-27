f8f451ce06b27999b21d20296c98f9b219886ed1
Andy Butland <abutland@zonecontent.com>
2015-03-27 23:29:00 +0100

    Added basket display to delivery page

--- MerchelloDemo.Web/Models/DeliveryPageViewModel.cs
+++ MerchelloDemo.Web/Models/DeliveryPageViewModel.cs
@@ -3,13 +3,32 @@
     using System;
     using System.ComponentModel.DataAnnotations;
     using System.Web.Mvc;
+    using MerchelloDemo.Web.Models.Interfaces;
 
-    public class DeliveryPageViewModel : BasePageViewModel
+    public class DeliveryPageViewModel : BasePageViewModel, IBasket
     {
         [Required(ErrorMessage = "Please select a delivery option")]
         [Display(Name = "Delivery option")]
         public Guid SelectedDeliveryOption { get; set; }
 
         public SelectList DeliveryOptions { get; set; }
+        
+        public BasketDetail BasketDetail { get; set; }
+
+        public bool AllowBasketEdit
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool ShowOrderTotal
+        {
+            get
+            {
+                return false;
+            }
+        }        
     }
 }
